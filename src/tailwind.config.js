4094f45f9f80adbafb1d89e97b22bacc86532ead
wendyhamel <whamel@it4web.nl>
2023-01-27 16:04:19 +0100

    Styling responsive card layout.

---
+++ src/tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+	theme: {
+		fontFamily : {
+			'sans': ['Barlow Semi Condensed', 'sans-serif'],
+		},
+		extend: {
+			colors: {
+				'violet': 'hsl(263, 55%, 52%)',
+				'blue-gray-very-dark': 'hsl(217, 19%, 35%)',
+				'blue-gray-light': 'hsl(210, 46%, 95%)',
+				'blue-black-very-dark': 'hsl(219, 29%, 14%)',
+				'light-gray': 'hsl(0, 0%, 81%)',
+				'white': 'hsl(0, 0%, 100%)',
+			},
+			fontSize: {
+				's': '0.8125rem',
+			},
+		}
+	}
+}
